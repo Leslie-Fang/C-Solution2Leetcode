2e3835ce64413a871b74fc892a5ad10befbfdde4
leslie-fang <fangjiahao@sjtu.edu.cn>
2017-07-18 20:50:51 +0800

    add 11 comment

--- 11ContainerWithMostWater/main.cpp
+++ 11ContainerWithMostWater/main.cpp
@@ -7,12 +7,14 @@
 using namespace std;
 
 int maxArea(vector<int>& height) {
+#first find the area of the widest area to contain the water
     int start=0,end=height.size()-1;
     int minus=min(height[start],height[end]);
     int mymax=minus*(end-start);
     while(1){
         minus=min(height[start],height[end]);
         mymax=max(mymax,minus*(end-start));
+#only if higher we need to calculate the water containing again
         while(height[start]<=minus && start<end){start++;}
         while(height[end]<=minus && start<end){end--;}
         if(start>=end){
