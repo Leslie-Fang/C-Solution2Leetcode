184beea40e1b7fcb64cc3cb36fc0cf98a84b6ef3
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-01-03 21:16:50 +0800

    add 6

--- 144BinaryTreePreorderTraversal/main.cpp
+++ 144BinaryTreePreorderTraversal/main.cpp
@@ -1,4 +1,42 @@
 //
 // Created by leslie-fang on 2017/8/13.
 //
+#include <iostream>
+#include <vector>
+
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+void getValue(TreeNode* node, vector<int> &res){
+    if(node){
+        res.push_back(node->val);
+    }
+    if(node->left){
+        getValue(node->left,res);
+    }
+    if(node->right){
+        getValue(node->right,res);
+    }
+    return;
+}
+
+vector<int> preorderTraversal(TreeNode* root) {
+    vector<int> res;
+    if(root){
+        getValue(root,res);
+    }
+    return res;
+}
+
+int main(){
+    cout<<"Hello world"<<endl;
+    TreeNode *root = new TreeNode(10);
+    cout<<root->val<<endl;
+    return 0;
+}
 
