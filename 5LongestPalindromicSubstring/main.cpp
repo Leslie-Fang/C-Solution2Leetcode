ad2992e69b7b5ea7bacd4cdd8565ea59504e02bb
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-08-05 10:31:11 +0800

    5 accept

--- 5LongestPalindromicSubstring/main.cpp
+++ 5LongestPalindromicSubstring/main.cpp
@@ -10,55 +10,39 @@ bool isPalindrome(string s);
 
 int main(){
 	Solution mySolution;
-	cout<<mySolution.longestPalindrome("babad")<<endl;
+	string input="";
+	// cout<<isPalindrome(input)<<endl;
+	cout<<mySolution.longestPalindrome(input)<<endl;
+	return 1;
 }
 
 string Solution::longestPalindrome(string s){
-	if(s.length() == 1){
-		return s;
-	}
-	string maxS=""+s[0];
-	int max=1;
-	int cmax=0;
-	int l =s.length();
-	int flag=1;
-	for (int i = 0; i < l; ++i){
-		cmax = 0;
-		flag=1;
-		for(int j=0;i-j>=0&&i+j<l;j++){
-			if(s[i-j] != s[i+j]){
-				if(cmax > max){
-					max = cmax;
-					maxS = s.substr(i-j+1,2*(j-1)+1);
-				}
-				flag=0;
-				break;
+	int l=s.length();
+	int count=l;
+	string subS;
+	while(count != 0){
+		for(int i=0;i<=(l-count);i++){
+			subS=s.substr(i,count);
+			if(isPalindrome(subS)){
+				return subS;
 			}
-			cmax = 2*j+1;
 		}
-		if(flag != 0){
-			cmax = 2*i-1;
-			max = cmax;
-			maxS = s;
-		}
-		cmax = 0;
-		flag =1;
-		for(int j=0;i-j>=0&&i+j<l;j++){
-			if(s[i-j] != s[i+j+1]){
-				if(cmax > max){
-					max = cmax;
-					maxS = s.substr(i-j+1,2*(j-1)+2);
-				}
-				flag=0;
-				break;
-			}
-			cmax = 2*j+2;
-		}
-		if(flag != 0){
-			cmax = 2*i;
-			max = cmax;
-			maxS = s;
-		}
+		count--;
 	}
-	return maxS;
+	return "";
 }
+
+bool isPalindrome(string s){
+	int i=0;
+	int j=s.length()-1;
+	while(i < j){
+		if(s[i] != s[j]){
+			return false;
+		}
+		i++;
+		j--;
+	}
+	return true;
+}
+
+
