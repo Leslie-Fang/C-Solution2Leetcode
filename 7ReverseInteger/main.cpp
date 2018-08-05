11304afd50d034f91ed15e120fb8faab74c7b134
leslie-fang <fangjiahao@sjtu.edu.cn>
2018-08-05 11:13:15 +0800

    7accept

--- 7ReverseInteger/main.cpp
+++ 7ReverseInteger/main.cpp
@@ -2,22 +2,44 @@
 // Created by leslie-fang on 2017/6/11.
 //
 #include <iostream>
-#include "reverseinteger.h"
 #include <math.h>
+#include <vector>
 using namespace std;
 
-int reverse(int x) {
-    long long res = 0;
-    while(x) {
-        res = res*10 + x%10;
-        x /= 10;
-    }
-    return (res<INT_MIN || res>INT_MAX) ? 0 : res;
+class Solution {
+public:
+    int reverse(int x);
+};
 
-
-}
 
 int main(){
-    cout<<reverse(10987890)<<endl;
+	Solution *mySolution = new Solution();
+	cout<<pow(2, 31)-1<<endl;
+    cout<<mySolution->reverse(10987890)<<endl;
     return 0;
+}
+
+int Solution::reverse(int x){
+	int upper = 2147483647;
+	int lower = -2147483648;
+    long res=0;
+    bool negative=false;
+    if(x<0){
+		negative=true;
+		x = -x;
+    }
+    while(1){
+    	res = res*10+x%10;
+    	x = x/10;
+    	if(x==0){
+    		break;
+    	}
+    }
+    if(negative){
+    	res =-res;
+    }
+    if(res < lower || res>upper){
+    	return 0;
+    }
+	return res;
 }
