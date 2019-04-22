576c9779130633958b4a81a9c51fe3b7e505d0fb
Leslie-Fang <fangjiahaosjtu2017@126.com>
2019-04-22 22:18:18 +0800

    48 accepy

--- 48RotateImage/main.cpp
+++ 48RotateImage/main.cpp
@@ -1,8 +1,33 @@
 #include <iostream>
+#include <vector>
 using namespace std;
+
+class Solution {
+public:
+    void rotate(vector<vector<int> >& matrix)
+    {
+		if (matrix.empty())
+			return;
+		for (int layer = 0; layer < matrix.size() / 2; ++layer)
+		{
+			int first = layer;
+			int last = matrix.size() - first - 1;
+			for (int i = first; i < last; ++i)
+			{
+				int tmp = matrix[first][i];
+				matrix[first][i] = matrix[last - i + layer][first];
+				matrix[last - i + layer][first] = matrix[last][last - i + layer];
+				matrix[last][last - i + layer] = matrix[i][last];
+				matrix[i][last] = tmp;
+			}
+		}
+    }
+};
 
 int main(int argc, char ** argv)
 {
-	cout<<"Hello"<<endl;
+	Solution * mySolution = new Solution();
+	vector<vector<int> > input;
+	mySolution->rotate(input);
 	return 0;
 }
