ba4a4bd6cf600b565d3cbc34e27f26eca5079df8
Leslie-Fang <fangjiahaosjtu2017@126.com>
2019-09-12 02:52:21 +0800

    14 accept

--- 14LongestCommonPrefix/main.cpp
+++ 14LongestCommonPrefix/main.cpp
@@ -10,43 +10,59 @@ public:
 
 int main(int argc,char** argv){
 	vector<string> input;
-	// input.push_back("dog");
-	// input.push_back("racecar");
-	// input.push_back("car");
-	input.push_back("flower");
-	input.push_back("flow");
-	input.push_back("flight");
+	input.push_back("dog");
+	input.push_back("racecar");
+	input.push_back("car");
+	// input.push_back("flower");
+	// input.push_back("flow");
+	// input.push_back("flight");
 	Solution * mySolution = new Solution();
 	cout<<mySolution->longestCommonPrefix(input)<<endl;
+	delete mySolution;
 	return 0;
 }
 string Solution::longestCommonPrefix(vector<string>& strs){
-	int myLength=strs[0].length();
-	vector<string>::iterator least;
-	for(vector<string>::iterator it=strs.begin();it != strs.end();it++){
-		if((*it).length()<myLength){
-			myLength=(*it).length();
-			least = it;
+	string res  = "";
+	if(strs.size() == 0)
+	{
+		return res;
+	}
+	int min_length = strs[0].length();
+	for(int i=0;i<strs.size();i++)
+	{
+		if(min_length < strs[i].length())
+		{
+			min_length = strs[i].length();
 		}
 	}
-	int res=myLength;
-	for(vector<string>::iterator it=strs.begin();it != strs.end();it++){
-		int subL=0;
-		for(int i=0;i<(*it).length();i++){
-			if((*least)[i] == (*it)[i]){
-				subL++;
+	if(min_length == 0)
+	{
+		return res;
+	}
+	int index = 0;
+	char judge = strs[0][index];
+	bool shouldContinue = true;
+	while(shouldContinue)
+	{
+		for(int i=0;i<strs.size();i++)
+		{
+			if(strs[i][index] != judge)
+			{
+				shouldContinue =false;
+				break;
 			}
 		}
-		if(subL<res){
-			res = subL;
+		if(shouldContinue)
+		{
+			res += judge;
+			index += 1;
+			if(index == min_length)
+			{
+				break;
+			}
+			judge = strs[0][index];
 		}
 	}
-	string sres="";
-	if(res==0){
-		return "";
-	}
-	for(int i=0;i<res;i++){
-		sres += (*least)[i];
-	}
-	return sres;
+	return res;
+
 }
