3794ca5d3eeeac31a44b3de499ef528e7c10d51d
Leslie-Fang <fangjiahaosjtu2017@126.com>
2020-01-27 23:24:23 +0800

    106 accept

--- 106ConstructBinaryTreefromInorderandPostorderTraversal/main.cpp
+++ 106ConstructBinaryTreefromInorderandPostorderTraversal/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
@@ -9,24 +8,15 @@ using namespace std;
      TreeNode *right;
      TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  };
- 
+
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
-    	if(preorder.size() > 0)
-    	{
-    		TreeNode * root = new TreeNode(preorder[0]);
-    		dfs(root, preorder, inorder, 0, preorder.size()-1, 0, inorder.size()-1);
-    		return root;
-    	}
-    	return NULL;
-    }
-    void dfs(TreeNode * root, vector<int>& preorder, vector<int>& inorder, int pn_start, int pn_end, int in_start, int in_end){
-    	if(pn_end <= pn_start)
+	void dfs(TreeNode * root, vector<int>& inorder, vector<int>& postorder,const int & in_start, const int & in_end, const int & po_start, const int& po_end){
+    	if(in_end <= in_start)
     	{
     		return;
     	}else
-    	{
+    	{    		
     		int in_left_index = in_start;
     		for(int i=in_start;i<=in_end;i++)
     		{
@@ -40,22 +30,31 @@ public:
     		int right_tree_number = in_end-in_left_index;
     		if(left_tree_number>=1)
     		{
-    			TreeNode * left_t = new TreeNode(preorder[pn_start+1]);
+    			TreeNode * left_t = new TreeNode(postorder[po_start+left_tree_number-1]);
     			root->left = left_t;
-    			dfs(left_t,preorder,inorder,pn_start+1,pn_start+left_tree_number,in_start,in_left_index-1);
+    			dfs(left_t,inorder,postorder,in_start,in_left_index-1,po_start,po_start+left_tree_number-1);
     		}
     		if(right_tree_number>=1)
     		{
-    			TreeNode * right_t = new TreeNode(preorder[pn_start+left_tree_number+1]);
+    			TreeNode * right_t = new TreeNode(postorder[po_end-1]);
     			root->right = right_t;
-    			dfs(right_t,preorder,inorder,pn_start+left_tree_number+1,pn_end,in_left_index+1,in_end);
+    			dfs(right_t,inorder,postorder,in_left_index+1,in_end,po_start+left_tree_number,po_start+left_tree_number+right_tree_number-1);
     		}
     	}
     	return;
+	}
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+    	if(postorder.size() > 0)
+    	{
+    		TreeNode * root = new TreeNode(postorder[postorder.size()-1]);
+    		dfs(root, inorder, postorder, 0, inorder.size()-1, 0, postorder.size()-1);
+    		return root;
+    	}
+    	return NULL;
     }
 };
 
-int main(int argc, char ** argv)
+ int main(int argc, char ** argv)
 {
 	#define number 3
 	int a[number] = {1,2,3};
@@ -63,7 +62,8 @@ int main(int argc, char ** argv)
 	vector<int> pn(a,a+number);
 	vector<int> in(b,b+number);
 	Solution * mySolution = new Solution();
-	mySolution->buildTree(pn,in);
+	mySolution->buildTree(in, pn);
 	delete mySolution;
 	return 0;
 }
+
